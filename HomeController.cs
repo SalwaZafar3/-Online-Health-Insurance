128f16fd93d17bcfbb410f967ece4a1100c1cd55
moiz <moiz1324@gmail.com>
2022-02-23 12:33:14 +0500

    ..
    ...

---
+++ HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult Policies()
+        {
+            return View();
+        }
+
+        public ActionResult Blog1()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Resources()
+        {
+            return View();
+        }
+    }
+}
